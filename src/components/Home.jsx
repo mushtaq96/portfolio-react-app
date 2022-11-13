2e86456edfcb12f7194a66eec47e478a71d89f4b
musthaq96 <mushtaq96smb@gmail.com>
2022-11-13 17:04:36 +0100

    fix spelling and add color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,9 +10,9 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='#16e0bd text-red-500'> Hi, my name is </p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#bbc4e2]'>SM Mushtaq Bokhari.</h1>
-                <h2 className='text-3xl sm:text-6xl font-bold text-[#8892b0]'>I build products for the web.</h2>
+                <h2 className='text-3xl sm:text-6xl font-bold text-[#8892b0]'>I build products for the &lt;<span className='text-red-500'>web</span>/&gt;.</h2>
                 <p className ='text-[#8892b0] py-4 max-w-[700px]'>
-                    I am a Full-stack deve  loper specializing in building robust applications, who is curious by nature and empathy minded.
+                    I am a Full-Stack developer specializing in building robust applications, who is curious by nature and empathy minded.
                     Currently a Software Engineering masters student seeking <span className='text-red-500'>Workstudent or Internship</span> opportunities. 
                     
                 </p>
