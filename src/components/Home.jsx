7a5422e35138fc8d461db469cad0cd55bf973229
mushtaq96 <mushtaq96smb@gmail.com>
2024-02-25 23:56:04 +0100

    update: home content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,11 +10,13 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='#16e0bd text-red-500'> Hi, my name is </p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#bbc4e2]'>SM Mushtaq Bokhari.</h1>
-                <h2 className='text-3xl sm:text-6xl font-bold text-[#8892b0]'>I build products for the &lt;<span className='text-red-500'>web</span>/&gt;.</h2>
+                <h2 className='text-3xl sm:text-5xl font-bold text-[#8892b0]'>I craft solutions for the AI &lt;<span className='text-red-500'>world</span>/&gt;.</h2>
                 <p className ='text-[#8892b0] py-4 max-w-[700px]'>
-                    I am a Full-Stack developer specializing in building robust applications, who is curious by nature and empathy minded.
-                    Currently a Software Engineering masters student seeking <span className='text-red-500'>Workstudent or Internship</span> opportunities. 
-                    
+                    I am a Full-Stack developer who is curious by nature, empathy minded, passionate about leveraging technology to solve real-world problems. 
+                    My focus lies in areas such as AI, machine learning, and data engineering. 
+                    Seeking <span className='text-red-500'>Workstudent or Full-Time</span> opportunities. 
+                    <br/>
+                   
                 </p>
                 <div>
                     <Link to="work" smooth={true} duration={500}>
