4b6441e3c551ed2e25841c62782e9cf599c8ae1b
mushtaq96 <mushtaq96smb@gmail.com>
2024-02-25 23:15:56 +0100

    update: about section, heading design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,21 @@ const About = () => {
         <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
             <div className='flex flex-col justify-center items-center w-full h-full'>
                 <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-                    <div className='sm:text-right pb-8 pl-4'>
-                        <p className='text-4xl font-bold inline border-b-4 border-red-600'>
-                            About
-                        </p>
+                    <div className='flex flex-col justify-center'>
+                       
+                            <p className='text-4xl font-bold inline border-b-4 border-red-600 text-right'>
+                                About
+                            </p>
+                       
                     </div>
-                   
+                    <div>
+                        {/* profile picture */}
+                        <img src={profileImage} alt='Profile' className='w-full max-w-xs rounded-full mx-auto mb-4' />
+                    </div>
                 </div>
-                <div className='max-w-[1000px] w-full grid grid-cols-1 sm:grid-cols-2 gap-8 px-4'>
+
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold'>
-                        <img src={profileImage} alt='Profile' className='w-48 h-48 rounded-full mb-4 ml-auto object-contain' />
                         <p>Born in India</p>
                         <p>Worked in Japan</p>
                         <p>Studying in Germany</p>
